2975c955655d9371289b2e0011bd1a42ea0ef83b
Luiz <luizrosalba@gmail.com>
2020-07-16 17:13:59 -0300

    adicionando curring

---
+++ curring.js
@@ -0,0 +1,28 @@
+///funcao sem curring 
+function soma(a,b) { 
+    return a+b; 
+}
+
+soma (2,2); 
+soma (2,3); 
+soma (2,4); 
+soma (2,5); 
+
+
+
+
+function somaComCurring(a) { 
+    return function(b){
+        return a+b;
+    }
+    
+}
+
+const soma2 = somaComCurring(2) ; 
+
+ console.log( soma2(2) ); 
+ console.log( soma2(3) ); 
+ console.log( soma2(4) ); 
+ console.log( soma2(5) ); 
+
+
