439ce836b3808a32f9679d6fa2bb147db412c90d
Luiz <luizrosalba@gmail.com>
2020-07-16 17:47:19 -0300

    adicionando Imutabilidade 2

---
+++ imutabilidade2.js
@@ -0,0 +1,26 @@
+const students = [
+    {
+        name: 'A',
+        grade: 7
+    },
+    {
+        name: 'B',
+        grade: 4
+    },
+    {
+        name: 'C',
+        grade: 10
+    }
+
+];
+
+function getApprovedStudents(studentsList){
+    return studentsList.filter(student => student.grade >=7); 
+}
+
+console.log('Alunos aprovados '); 
+console.log(getApprovedStudents(students)); 
+
+console.log('\n Lista de Alunos '); 
+console.log(students); 
+
